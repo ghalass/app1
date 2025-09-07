0ce170722a94eca10ddeb03534acf79abcaabef7
MED SALEM GHALASS <msghalas@gmail.com>
2025-09-07 17:40:02 +0000

    aa

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,32 +3,27 @@ set -e
 
 echo "Deployment started ..."
 
+
 # Make sure NVM is available
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Reset and update code, remove untracked files
-echo "Fetching latest code..."
-git fetch origin main
-git reset --hard origin/main
-git clean -fd
-echo "Code updated from remote!"
+# Pull the latest version of the app
+git pull origin main # Your branch name
+echo "New changes copied to server !"
 
 # Installing Dependencies
 echo "Installing Dependencies..."
 npm install --yes
 
-# Creating a build with env variables
-echo "Setting environment variables..."
-export VITE_BASE_URL="https://jsonplaceholder.typicode.com/todos"
-export VITE_NODE_ENV="production"
-
-echo "Building application..."
+# Creating a build 
+echo "Building application"
 npm run build
 
-# Copying dist to /var/www/app1
+# Copying dist to /var/www/[project name]
 echo "Copying dist to /var/www/app1"
-sudo rm -rf /var/www/app1/*
 sudo cp -r dist/* /var/www/app1
+
+
 
 echo "Deployment Finished!"
